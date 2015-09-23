cd91819b4b4bb5f5b976200228481d35b138bd1f
league-workshopper-8 <workshop@MacBook-Pro.local>
2015-09-23 15:01:45 -0700

    loop da loop

---
+++ src/day1/robot/MultiColoredSquare.java
@@ -0,0 +1,23 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class MultiColoredSquare {
+public static void main(String[] args) {
+	Robot R2D2=new Robot();
+	R2D2.setSpeed(100);
+		for (int i = 0; i < 100; i++) {
+			
+			R2D2.setRandomPenColor();
+		for (int j = 0; j < 4; j++) {
+			
+		
+		R2D2.penDown();
+		R2D2.setPenWidth(10);
+		R2D2.move(100);
+		R2D2.turn(90);
+	
+		}
+		}
+}
+}
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,5 +1,7 @@
 package day1.robot;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
@@ -7,8 +9,16 @@ public class MyFirstJavaProgram {
 		
 		// START HERE
 
-		new Robot();
-		
+		Robot LaShawna=new Robot();
+		for (int i = 0; i < 30; i++) {
+			
+		LaShawna.setSpeed(100);
+		LaShawna.penDown();
+		LaShawna.setRandomPenColor();
+		LaShawna.move(300);
+		LaShawna.turn(180);
 		
 	}
 }
+
+	}
