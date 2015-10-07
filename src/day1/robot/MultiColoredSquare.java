a080f2769a6585207574701a9b31849be89be301
league-workshopper-1 <workshop@Mac-mini.local>
2015-10-07 15:01:15 -0700

    Followed recipe to create multi colored spiral, pentagon, etc

--- src/day1/robot/MultiColoredSquare.java
+++ src/day1/robot/MultiColoredSquare.java
@@ -3,21 +3,21 @@ package day1.robot;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MultiColoredSquare {
-public static void main(String[] args) {
-	Robot R2D2=new Robot();
-	R2D2.setSpeed(100);
+	public static void main(String[] args) {
+		Robot R2D2 = new Robot();
+		R2D2.setSpeed(600);
 		for (int i = 0; i < 100; i++) {
-			
+
 			R2D2.setRandomPenColor();
-		for (int j = 0; j < 4; j++) {
-			
-		
-		R2D2.penDown();
-		R2D2.setPenWidth(30);
-		R2D2.move(100);
-		R2D2.turn(90);
-	
+			for (int j = 0; j < 4; j++) {
+
+				R2D2.penDown();
+				R2D2.setPenWidth(30);
+				R2D2.move(100);
+				R2D2.turn(90);
+
+			}
 		}
-		}
-}
+		R2D2.sparkle();
+	}
 }
